77457564619db3d7a2a91044040d8e50a0456148
Konrad Karpiuk <01178228@pw.edu.pl>
2023-05-02 20:45:44 +0200

    Changed resolution of a screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,23 @@
-int main() { return 0;}
+#include <SFML/Graphics.hpp>
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1920,1080),
+    "First window!");
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed)
+            {
+            // Close window button clicked.
+            window.close();
+            }
+    }
+        window.clear(sf::Color::Black);
+        // Draw here.
+        window.display();
+    }
+    return 0;
+}
