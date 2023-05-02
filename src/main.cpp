c83d02bff61dced6b17370736ab31e9dc25b76d4
Konrad Karpiuk <01178228@pw.edu.pl>
2023-05-02 21:50:11 +0200

    Added textures directory

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,43 @@
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1920,1080),
-    "First window!");
+    "Bouncing Logosle!");
+    sf::Texture texture;
+    texture.loadFromFile("../src/textures/Logosle.png");
+    sf::Sprite Logosle(texture);
+    sf::Vector2u size = texture.getSize();
+    Logosle.setOrigin(size.x / 2, size.y / 2);
+    sf::Vector2f increment(0.4f, 0.4f);
     while(window.isOpen())
     {
         sf::Event event;
-        while(window.pollEvent(event)){
+        while(window.pollEvent(event))
+        {
             if(event.type == sf::Event::Closed)
             {
             // Close window button clicked.
             window.close();
             }
-    }
-        window.clear(sf::Color::Black);
-        // Draw here.
+        }
+        if((Logosle.getPosition().x + (size.x / 2) >
+        window.getSize().x && increment.x > 0) ||
+        (Logosle.getPosition().x - (size.x / 2) < 0 &&
+        increment.x < 0))
+        {
+        // Reverse the direction on X axis.
+        increment.x = -increment.x;
+        }
+        if((Logosle.getPosition().y + (size.y / 2) >
+        window.getSize().y && increment.y > 0) ||
+        (Logosle.getPosition().y - (size.y / 2) < 0 &&
+        increment.y < 0))
+        {
+        // Reverse the direction on Y axis.
+        increment.y = -increment.y;
+        }
+        Logosle.setPosition(Logosle.getPosition() + increment);
+        window.clear(sf::Color(16,16,16,255)); // Dark gray.
+        window.draw(Logosle); // Drawing our sprite.
         window.display();
     }
-    return 0;
 }
