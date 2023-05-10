5921cade3be6b55083d8500491725921c8a8f0a9
Kamil Cisek <01178216@pw.edu.pl>
2023-05-10 21:45:52 +0200

    Feature for updating state in Board class added

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,15 +1,56 @@
 #include "Board.h"
+#include "SolidObjects/Baba.h"
 
 Board::Board(const ObjectsOnFieldPtr3Vector &objectsOnField)
 {
     _objectsOnField = objectsOnField;
+    _xSize = objectsOnField.size();
+    _ySize = objectsOnField[0].size();
 }
 
-Board::Board() {}
+Board::Board(int x, int y)
+{
+    _xSize = x;
+    _ySize = y;
+
+    // Resize vector for speed and memory efficiency
+    // Resize x dimension
+    _objectsOnField.resize(x);
+    // Resize y dimension
+    for (int i = 0; i < x; i++) {
+        _objectsOnField[i].resize(y);
+    }
+    // Resize z dimension
+    for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {
+            _objectsOnField[i][j].resize(2);
+        }
+    }
+}
+
+void Board::updateState(int action)
+{
+    for (const Coordinates& object_coordinates : getYouObjectsCoordinates()) {
+        if (action == actions::UP) {
+            moveUp(object_coordinates.x, object_coordinates.y, object_coordinates.z);
+        } else if (action == actions::DOWN) {
+            moveDown(object_coordinates.x, object_coordinates.y, object_coordinates.z);
+        } else if (action == actions::LEFT) {
+            moveLeft(object_coordinates.x, object_coordinates.y, object_coordinates.z);
+        } else if (action == actions::RIGHT) {
+            moveRight(object_coordinates.x, object_coordinates.y, object_coordinates.z);
+        }
+    }
+}
 
 ObjectOnFieldPtr Board::getObject(int x, int y, int z) const
 {
     return _objectsOnField[x][y][z];
+}
+
+ObjectOnFieldPtr Board::getObject(const Coordinates &coordinates) const
+{
+    return _objectsOnField[coordinates.x][coordinates.y][coordinates.z];
 }
 
 ObjectOnFieldPtr Board::getObject(int x, int y) const
@@ -17,19 +58,42 @@ ObjectOnFieldPtr Board::getObject(int x, int y) const
     return _objectsOnField[x][y][0];
 }
 
+int Board::getXSize() const
+{
+    return _xSize;
+}
+
+int Board::getYSize() const
+{
+    return _ySize;
+}
+
 void Board::removeObject(int x, int y, int z)
 {
-    if (_objectsOnField[x][y].size() == 1) {
-        _objectsOnField[x][y][0] = nullptr;
+    if (_objectsOnField[x][y][1] == nullptr ||
+        (_objectsOnField[x][y][1] != nullptr && z == 1)) {
+        _objectsOnField[x][y][z] = nullptr;
+    }
+    else if (_objectsOnField[x][y][1] != nullptr && z == 0) {
+        _objectsOnField[x][y][0] = _objectsOnField[x][y][1];
+        _objectsOnField[x][y][1] = nullptr;
     }
     else {
-        _objectsOnField[x][y].erase(_objectsOnField[x][y].begin() + z);
+        std::cerr << "Ivalid coordinates!" << std::endl;
     }
 }
 
-void Board::addObject(int x, int y, const ObjectOnFieldPtr &object)
+void Board::addObject(int x, int y, const ObjectOnFieldPtr &object_ptr)
 {
-    _objectsOnField[x][y].emplace_back(object);
+    if (_objectsOnField[x][y][0] == nullptr) {
+        _objectsOnField[x][y][0] = object_ptr;
+    }
+    else if (_objectsOnField[x][y][0] != nullptr && _objectsOnField[x][y][1] == nullptr) {
+        _objectsOnField[x][y][1] = object_ptr;
+    }
+    else {
+        std::cerr << "3 objects cannot be in the same field!" << std::endl;
+    }
 }
 
 void Board::moveUp(int x, int y, int z)
@@ -54,4 +118,27 @@ void Board::moveRight(int x, int y, int z)
 {
     addObject(x+1, y, _objectsOnField[x][y][z]);
     removeObject(x, y, z);
+}
+
+std::vector<Coordinates> Board::getYouObjectsCoordinates() const
+{
+    std::vector<Coordinates> youObjectsCoordinates;
+
+    // Iterate over vector
+    for (int x = 0; x < _objectsOnField.size(); x++) {
+        for (int y = 0; y < _objectsOnField[x].size(); y++) {
+            for (int z = 0; z < _objectsOnField[x][y].size(); z++) {
+                // Check if object has isYou flag
+                if (_objectsOnField[x][y][z] != nullptr && _objectsOnField[x][y][z]->isYou) {
+                        Coordinates coordinates;
+                        coordinates.x = x;
+                        coordinates.y = y;
+                        coordinates.z = z;
+                        youObjectsCoordinates.emplace_back(coordinates);
+                }
+            }
+        }
+    }
+
+    return youObjectsCoordinates;
 }
--- src/Board.h
+++ src/Board.h
@@ -5,30 +5,63 @@
 #include <memory>
 
 #include "ObjectOnField.h"
+#include "SolidObject.h"
 
 using ObjectOnFieldPtr = std::shared_ptr<ObjectOnField>;
 using ObjectsOnFieldPtr3Vector = std::vector<std::vector<std::vector<ObjectOnFieldPtr>>>;
 
+enum actions { UP, DOWN, LEFT, RIGHT };
+
+struct Coordinates
+{
+    int x;
+    int y;
+    int z;
+};
+
 class Board
 {
 private:
-    /* Three dimensional vector of shared pointers to the ObjectOnField class instances.
+    /** Three dimensional vector of shared pointers to the ObjectOnField class instances.
     * Third dimension is to allow two objects occupying the same field. */
     ObjectsOnFieldPtr3Vector _objectsOnField;
 
+    /** Size of board */
+    int _xSize, _ySize;
+
 public:
     Board(const ObjectsOnFieldPtr3Vector &objectsOnField);
-    Board();
 
+    /** @param x First dimension size.
+     * @param y Second dimension size.
+     * Third dimension size is set to 2.  */
+    Board(int x, int y);
+
+    /** Function which takes user action and do everything to update board state
+    * properly after that action.
+    * @param action Integer from actions enum. */
+    void updateState(int action);
+
+    /** Getters */
     ObjectOnFieldPtr getObject(int x, int y, int z) const;
+    ObjectOnFieldPtr getObject(const Coordinates &coordinates) const;
     ObjectOnFieldPtr getObject(int x, int y) const;
+    int getXSize() const;
+    int getYSize() const;
 
+    /** Remove object - replace it nullptr*/
     void removeObject(int x, int y, int z);
 
+    /** Add object to board */
     void addObject(int x, int y, const ObjectOnFieldPtr &object);
 
+    /** Check if move is possible and move if it is possible. */
     void moveUp(int x, int y, int z);
     void moveDown(int x, int y, int z);
     void moveLeft(int x, int y, int z);
     void moveRight(int x, int y, int z);
+
+    /** Finds solid objects which have isYou flag set to true from objects on board
+     * and returns their coordinates. */
+    std::vector<Coordinates> getYouObjectsCoordinates() const;
 };
