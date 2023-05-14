72880e4ae5041fa58bfd568beb87d62c361cf077
Kamil Cisek <01178216@pw.edu.pl>
2023-05-14 21:07:07 +0200

    Paths to images added

---
+++ src/paths.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+
+const std::map<std::string, std::string> paths = {
+    {"Empty", "textures/Empty.png"},
+    {"Baba", "textures/Baba.png"},
+    {"BabaUp", "textures/BabaUp.png"},
+    {"BabaDown", "textures/BabaDown.png"},
+    {"BabaLeft", "textures/BabaLeft.png"},
+    {"BabaRight", "textures/BabaRight.png"},
+    {"Flag", "textures/Flag.png"},
+    {"Rock", "textures/Rock.png"},
+    {"Wall", "textures/Wall.png"},
+    {"Water", "textures/Water.png"},
+    {"Lava", "textures/Lava.png"},
+    {"Skull", "textures/Skull.png"},
+    {"Grass", "textures/Grass.png"},
+};
