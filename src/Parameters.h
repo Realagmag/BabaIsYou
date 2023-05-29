41aeeff0f1cef188bc9fa06db34d7cdcbe58762c
Kamil Cisek <01178216@pw.edu.pl>
2023-05-29 09:36:42 +0200

    Parameters changed

--- src/Parameters.h
+++ src/Parameters.h
@@ -8,7 +8,7 @@ struct Parameters
     // Number of archival states to store
     static const unsigned int MAX_ARCHIVAL_STATES;
 
-    // Number of levels in the game
+    // Number of levels in the game, now max 8
     static const unsigned int NUMBER_OF_LEVELS;
 
     // Paths to textures
